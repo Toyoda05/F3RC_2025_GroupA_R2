947103877dfe8a80a7bca5255847d03fd94c162b
Toyoda05 <d_toyoda@keio.jp>
2025-08-04 11:58:33 +0900

    feat: add motor pin name

--- include/config.hpp
+++ include/config.hpp
@@ -1,6 +1,26 @@
 #pragma once
 #include <PinNames.h>
 
+namespace DigitalOutPins
+{
+    constexpr PinName OMUNI_MOTOR1_DIR  = PB_10;     // オムニホイールDC1の方向
+    constexpr PinName OMUNI_MOTOR2_DIR  = PC_7 ;     // オムニホイールDC2の方向
+    constexpr PinName OMUNI_MOTOR3_DIR  = PB_4 ;     // オムニホイールDC3の方向
+    constexpr PinName PSEUDO_SERVO1_DIR = PB_9 ;     // 疑似サーボ用DC1の方向
+    constexpr PinName PSEUDO_SERVO1_DIR = PB_6 ;     // 疑似サーボ用DC2の方向
+    constexpr PinName PSEUDO_SERVO1_DIR = PA_4 ;     // 疑似サーボ用DC3の方向
+}
+
+namespace PwmOutPins
+{
+    constexpr PinName OMUNI_MOTOR1_PWM  = PA_8 ;     // オムニホイールDC1のPWM (Tim1)
+    constexpr PinName OMUNI_MOTOR2_PWM  = PA_9 ;     // オムニホイールDC2のPWM (Tim1)
+    constexpr PinName OMUNI_MOTOR3_PWM  = PA_10;     // オムニホイールDC3のPWM (Tim1)
+    constexpr PinName PSEUDO_SERVO1_PWM = PA_6 ;     // 疑似サーボ用DC1のPWM (Tim3)
+    constexpr PinName PSEUDO_SERVO1_PWM = PA_7 ;     // 疑似サーボ用DC2のPWM (Tim3)
+    constexpr PinName PSEUDO_SERVO1_PWM = PB_0 ;     // 疑似サーボ用DC3のPWM (Tim3)
+}
+
 namespace InterruptInPins
 {
     constexpr PinName MEASURING_ENCODER1_A      = PC_10;     // エンコーダー1：測定輪エンコーダ1のA相
